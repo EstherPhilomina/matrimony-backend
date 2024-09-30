3fd5bf5ae4d521259a842eb9abd563873b465ddf
Philomina <estherphilominas@gmail.com>
2024-09-30 11:57:57 +0530

    update on backend code

--- src/main/java/io/gemsproject/matrimony/registration/Registration.java
+++ src/main/java/io/gemsproject/matrimony/registration/Registration.java
@@ -1,7 +1,9 @@
 package io.gemsproject.matrimony.registration;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "Registration")
 public class Registration {
 	
 	@Id
